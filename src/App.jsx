dad85a610d789a58be80fa43a4dd73466369da33
Md-sarfraz <mdsarfraz62070@gmail.com>
2025-01-11 21:20:34 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
      <h1 className="text-3xl text-red-500 text-center mt-5">Real Time Chat Application</h1>
+     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non beatae, nesciunt natus incidunt aspernatur eaque iusto culpa animi nihil explicabo tempora velit maiores omnis nostrum odio totam libero perferendis laboriosam?</p>
     </>
   )
 }
