5a9d35ba0110e61fa8440d0881a48173967f4228
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2025-01-11 21:06:25 +0530

    frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-3xl text-red-500 text-center mt-5">Real Time Chat Application</h1>
+    </>
+  )
+}
+
+export default App
